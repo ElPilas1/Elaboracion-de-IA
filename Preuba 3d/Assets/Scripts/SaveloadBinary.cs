43e6bd21353de366d8c73efe62e71e4f394027b0
ElPilas1 <dieya2003@gmail.com>
2024-09-30 13:51:34 +0200

    Cambios 3era cosa
    Solo queda hacer la movida de el texto

---
+++ Preuba 3d/Assets/Scripts/SaveloadBinary.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;//IO SIGNIFICA INPUT OUTPUT
+using UnityEngine;
+
+public class SaveloadBinary : MonoBehaviour
+{
+    public string filename = "test.pdf";
+    // Start is called before the first frame update
+    void Start()
+    {
+        filename=Application.persistentDataPath + "//" + filename;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if(Input.GetKeyDown(KeyCode.G))
+        {
+            Save();
+        }
+        else if (Input.GetKeyDown(KeyCode.L))
+        {
+            Load();
+        }
+    }
+
+    void Save()
+    {
+
+        BinaryWriter binaryWriter= new BinaryWriter(new FileStream(filename,FileMode.Create));
+        binaryWriter.Write(transform.position.x);
+        binaryWriter.Write(transform.position.y);
+        binaryWriter.Write(transform.position.z);
+        binaryWriter.Flush();//para limpiar la caca y el flujo del vater
+        binaryWriter.Close();
+    }
+    void Load()
+    {
+        if (File.Exists(filename))
+        {
+          return;
+        }
+        BinaryReader binaryReader= new BinaryReader(new FileStream(filename,FileMode.Open));//forma de hacerlo mas profesional con el return
+        float x = binaryReader.ReadSingle();
+        float y = binaryReader.ReadSingle();
+        float z = binaryReader.ReadSingle();
+        binaryReader.Close();
+
+        transform.position = new Vector3(x, y, z);
+    }
+
+}
--- Preuba 3d/Assets/Scripts/Updatetext.cs
+++ Preuba 3d/Assets/Scripts/Updatetext.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Hud : MonoBehaviour
+public class Updatetext : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-       
+        
     }
 
     // Update is called once per frame
