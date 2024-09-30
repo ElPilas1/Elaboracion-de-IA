fd0f2c32f1d4f879e214dfda8ba1be9814cdf45e
ElPilas1 <dieya2003@gmail.com>
2024-09-30 14:08:03 +0200

    Update Updatetext.cs

--- Preuba 3d/Assets/Scripts/Updatetext.cs
+++ Preuba 3d/Assets/Scripts/Updatetext.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Updatetext : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public class UpdateText : MonoBehaviour
     {
-        
-    }
+        public GameManager.GameManagerVariables variable; //actualiza el texto a la variable de GameManager que le indiquemos.
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private TMP_Text textComponent;
+
+        private void Start()
+        {
+            textComponent = GetComponent<TMP_Text>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            switch (variable)
+            {
+                case GameManager.GameManagerVariables.TIME:
+                    textComponent.text = "Time: " + GameManager.instance.GetTime(); //lO NARANJA SE LLAMA CONNATENACION.
+                    break;
+                case GameManager.GameManagerVariables.POINTS:
+                    textComponent.text = "Points: " + GameManager.instance.GetPoints();
+                    break;
+                default:
+                    break;
+            }
+        }
     }
-}
