8f213fb5b508d2ecfc4d7287ca03800aeeb753e2
ElPilas1 <dieya2003@gmail.com>
2024-10-28 09:47:02 +0100

    SeeAction para disparo

---
+++ Preuba 3d/Assets/Scripts/SeeActions.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SeeAction (A)", menuName = "ScriptableObject/Action/SeeActions")]//IMPORTANTE
+public class SeeActions : Action
+{
+    public float AngleVision; //el angulo que le das a la vista (mas o menos la vista es de 100)
+    public override bool Check(GameObject owner)
+    {
+        GameObject target = owner.GetComponent<Targetreference>().target;
+        Vector3 dir = target.transform.position - owner.transform.position;//la direccion donde se va la vista
+
+        float angle = Vector3.Angle(dir, owner.transform.forward);//Coge el angulo del player y donde esta el enemigo
+
+        Debug.DrawRay(owner.transform.position + owner.transform.up, dir);
+
+        if (angle < AngleVision * 0.5f)//si el angulo que se ve  es menor al angulo de vision
+        {
+            RaycastHit hits;//el raycast se utiliza si le da
+
+
+            if (Physics.Raycast(owner.transform.position + owner.transform.up, dir.normalized, out hits, 30f))//te mira si estas dando el raycast
+            {
+
+                if (hits.collider.gameObject == target)//si pegas o tocas al  taget 
+                {
+                    return true; //lo ve
+                }
+
+            }
+
+        }
+
+
+        // RaycastHit[] hits = Physics.RaycastAll(owner.transform.position, Vector3.forward, 30f);//
+        // GameObject target = owner.GetComponent<Target>().target;
+
+        return false;
+    }
+
+    public override void DrawGizmo(GameObject owner)
+    {
+
+    }
+}
